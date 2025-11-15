3e6c14fb13a3dae6264d20842842875f67f1591d
Rami Mohamed <eng.ramimoha@gmail.com>
2025-11-15 16:40:21 +0800

    fixed front-end structure and added initial implementation for login
    - added routing logic
    - fixed the folder structure for the front end
    - added initial implementation for login page

---
+++ app/frontend/fe-nabdtwinapp/src/services/router.tsx
@@ -0,0 +1,12 @@
+import {Route , createBrowserRouter , createRoutesFromElements , RouterProvider} from 'react-router-dom'
+import LoginPage from "../pages/LoginPage.tsx";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/">
+            <Route index element={<LoginPage />} />
+        </Route>
+    )
+);
+
+export  default router ;
