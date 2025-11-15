9bb4652505347e4dff992984e000222031c3874b
Rami Mohamed <eng.ramimoha@gmail.com>
2025-11-15 16:40:21 +0800

    fixed front-end structure and added initial implementation for login
    - added routing logic
    - fixed the folder structure for the front end
    - added initial implementation for login page

---
+++ app/frontend/fe-nabdtwinapp/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+
+function LoginPage() {
+    return (
+        <>
+            <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+                <h1>hi</h1>
+            </div>
+        </>
+    );
+}
+
+export default LoginPage;
